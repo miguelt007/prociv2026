cfb519070ea282477afc8e79764c48697b1e5b37
Miguel Teixeira <miguelt007@gmail.com>
2026-02-12 21:02:21 +0000

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,13 +5,19 @@ const API_URL = "https://services-eu1.arcgis.com/VlrHb7fn5ewYhX6y/arcgis/rest/se
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(API_URL, {
+      headers: {
+        "Accept": "application/json",
+        "User-Agent": "VercelServer"
+      }
+    });
+    
     if (!response.ok) throw new Error(`Erro API: ${response.status}`);
     const data = await response.json();
 
     res.status(200).json(data); // envia direto para o front-end
   } catch (error) {
-    console.error(error);
+    console.error("Proxy failed:", error.message);
     res.status(500).json({ error: "Erro ao buscar dados da API" });
   }
 }
