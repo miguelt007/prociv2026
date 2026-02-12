341caccda1e1d6cc3fec360867765c5ee987b60e
Miguel Teixeira <miguelt007@gmail.com>
2026-02-12 20:04:38 +0000

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,17 +2,18 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
     try {
-        // URL da API com limite grande para retornar todas as ocorrências
-        const API_URL = "https://services-eu1.arcgis.com/VlrHb7fn5ewYhX6y/arcgis/rest/services/OcorrenciasSite/FeatureServer/0/query?f=geojson&where=0=0&outFields=*&resultRecordCount=10000";
+        // URL correta da API
+        const API_URL = "https://services-eu1.arcgis.com/VlrHb7fn5ewYhX6y/arcgis/rest/services/OcorrenciasSite/FeatureServer/0/query?f=geojson&where=1=1&outFields=*";
 
         const response = await fetch(API_URL);
+
         if (!response.ok) {
             return res.status(response.status).json({ error: "Erro na API original" });
         }
 
         const data = await response.json();
 
-        // Retorna o JSON intacto para o frontend
+        // Retorna JSON para o frontend
         res.status(200).json(data);
 
     } catch (error) {
